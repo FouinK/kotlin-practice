5e74d9406be1187b02a1dbba67fe8eebcc7842db
Fouink <yyss9103@gmail.com>
2023-09-09 16:06:28 +0900

    first commit

---
+++ src/main/kotlin/com/example/firstKotlin/FirstKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.firstKotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FirstKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<FirstKotlinApplication>(*args)
+}
