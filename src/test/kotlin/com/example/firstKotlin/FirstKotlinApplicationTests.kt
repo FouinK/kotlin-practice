5fa7ac48e39efab9b1936018b0706d6f8b718782
Fouink <yyss9103@gmail.com>
2023-09-10 18:39:22 +0900

    when 안에 is 처럼 타입 검사하는 것 등등 모든 조건부는 가능 2

--- src/test/kotlin/com/example/firstKotlin/FirstKotlinApplicationTests.kt
+++ src/test/kotlin/com/example/firstKotlin/FirstKotlinApplicationTests.kt
@@ -7,14 +7,14 @@ import org.springframework.boot.test.context.SpringBootTest
 class FirstKotlinApplicationTests {
 	@Test
 	fun contextLoads() {
-		println(getGradeWithSwitch("A성현"))
+		println(getGradeWithSwitch(1))
 	}
 
 
-	fun getGradeWithSwitch(obj: Any): Boolean {
-		return when (obj) {
-			is String -> obj.startsWith("A")
-			else -> false
+	fun getGradeWithSwitch(number: Int): String {
+		return when (number) {
+			-1, 0, 1 -> "어디서 많이 본 숫자입니다"
+			else -> "-1, 0, 1이 아닙니다"
 		}
 	}
 }
