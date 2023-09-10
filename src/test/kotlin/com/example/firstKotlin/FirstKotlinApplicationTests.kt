d05ebb9162d188500af2d32fe19db8f83874ed2a
Fouink <yyss9103@gmail.com>
2023-09-10 18:29:16 +0900

    범위 조건 in

--- src/test/kotlin/com/example/firstKotlin/FirstKotlinApplicationTests.kt
+++ src/test/kotlin/com/example/firstKotlin/FirstKotlinApplicationTests.kt
@@ -2,19 +2,23 @@ package com.example.firstKotlin
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import java.lang.IllegalArgumentException
+import java.util.IllegalFormatException
 
 @SpringBootTest
 class FirstKotlinApplicationTests {
 	@Test
 	fun contextLoads() {
-		println(myif(null))
+		println(testFor(1000))
 	}
 
-	fun myif(str: String?): String {
-		return if (str == "A") {
-			"A"
+	fun testFor(score: Int): Boolean {
+		if (score !in 0..100) {
+			return false
+		} else if (score in 0..100) {
+			return true
 		} else {
-			"B"
+			throw IllegalArgumentException("스코어의 범위를 특정할 수 없습니다")
 		}
 	}
 }
