7045325e045683b876e1c8ab121ebdf77f23d29c
Fouink <yyss9103@gmail.com>
2023-09-10 18:36:29 +0900

    when 안에 is 처럼 타입 검사하는 것 등등 모든 조건부는 가능

--- src/test/kotlin/com/example/firstKotlin/FirstKotlinApplicationTests.kt
+++ src/test/kotlin/com/example/firstKotlin/FirstKotlinApplicationTests.kt
@@ -2,22 +2,19 @@ package com.example.firstKotlin
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-import java.lang.IllegalArgumentException
-import java.util.IllegalFormatException
 
 @SpringBootTest
 class FirstKotlinApplicationTests {
 	@Test
 	fun contextLoads() {
-		println(getGradeWithSwitch(1000))
+		println(getGradeWithSwitch("A성현"))
 	}
 
 
-	fun getGradeWithSwitch(score: Int): String {
-		return when (score / 10) {
-			9 -> "A"
-			in 7..8 -> "B"
-			else -> "C"
+	fun getGradeWithSwitch(obj: Any): Boolean {
+		return when (obj) {
+			is String -> obj.startsWith("A")
+			else -> false
 		}
 	}
 }
