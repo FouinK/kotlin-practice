6d06cc6d273292b6f7818e6880030c7684d67651
Fouink <yyss9103@gmail.com>
2023-09-10 12:29:21 +0900

    타입 변환 방식들

--- src/test/kotlin/com/example/firstKotlin/FirstKotlinApplicationTests.kt
+++ src/test/kotlin/com/example/firstKotlin/FirstKotlinApplicationTests.kt
@@ -7,7 +7,29 @@ import org.springframework.boot.test.context.SpringBootTest
 class FirstKotlinApplicationTests {
 	@Test
 	fun contextLoads() {
-		val str: String? = null
-		println(str!!.length)
+		val person = Person("성현")
+		printAgeIfPerson(person)
 	}
+
+	fun printAgeIfPerson(obj: Any?) {
+		val person = obj as? Person
+		println(person?.name)
+
+		if (obj is Person) {
+			val person1 = obj as Person
+			println(person1.name)
+		}
+
+		if (obj is Person) {
+			println(obj.name)
+		}
+	}
+
+	 class Person {
+		 var name: String
+
+		 constructor(name: String) {
+			 this.name = name
+		 }
+	 }
 }
