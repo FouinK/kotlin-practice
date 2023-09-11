d945bc3a8190968502ada96e4a18d36bd24d1679
Fouink <yyss9103@gmail.com>
2023-09-11 20:47:17 +0900

    클래스 생성자에 val, var을 명시하면 자동으로 getter setter를 만들어줌

--- src/main/kotlin/com/example/firstKotlin/Person.kt
+++ src/main/kotlin/com/example/firstKotlin/Person.kt
@@ -3,8 +3,8 @@ package com.example.firstKotlin
 import java.lang.IllegalArgumentException
 
 class Person(
-    val name: String = "윤성현",
-    var age: Int = 1
+    val name: String = "윤성현",       //val, var을 붙이면 자동으로 게터 세터를 만들어줌 커스텀 게터 세터를 만들고 싶으면 val, var을 뗄 것
+    var age: Int = 1        //val, var을 붙이면 자동으로 게터 세터를 만들어줌 커스텀 게터 세터를 만들고 싶으면 val, var을 뗄 것
 ) {
     init {
         if (age <= 0) {
