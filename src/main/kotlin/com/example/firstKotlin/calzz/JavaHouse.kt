6972857390fb91465a4b22bb1b017f6d5a35d57d
Fouink <yyss9103@gmail.com>
2023-09-11 22:30:54 +0900

    기본적인 중첩 클래스 사용법 (응답 DTO, 요청 DTO 사용할 떄 쓸 것)

---
+++ src/main/kotlin/com/example/firstKotlin/calzz/JavaHouse.kt
@@ -0,0 +1,10 @@
+package com.example.firstKotlin.calzz
+
+class JavaHouse (
+    private val address: String,
+    private val livingRoom: LivingRoom
+) {
+    class LivingRoom(
+        private val area: Double
+    )
+}
