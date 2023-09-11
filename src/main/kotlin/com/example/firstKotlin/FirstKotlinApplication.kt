dfc29cb9aedf169e7786048b6391edaed72ee219
Fouink <yyss9103@gmail.com>
2023-09-11 22:19:35 +0900

    직접적인 스태틱 객체 만드는법

--- src/main/kotlin/com/example/firstKotlin/FirstKotlinApplication.kt
+++ src/main/kotlin/com/example/firstKotlin/FirstKotlinApplication.kt
@@ -3,6 +3,7 @@ package com.example.firstKotlin
 import com.example.firstKotlin.calzz.Derived
 import com.example.firstKotlin.calzz.People
 import com.example.firstKotlin.calzz.Person
+import com.example.firstKotlin.calzz.Singleton
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -17,6 +18,9 @@ fun main(args: Array<String>) {
 	People.newBaby("이름")
 	People.Factory.log()
 
+	println("Singleton.a = ${Singleton.a}")
+	Singleton.a += 10
+	println("Singleton.a = ${Singleton.a}")
 
 	runApplication<FirstKotlinApplication>(*args)
 }
---
+++ src/main/kotlin/com/example/firstKotlin/calzz/Singleton.kt
@@ -0,0 +1,5 @@
+package com.example.firstKotlin.calzz
+
+object Singleton  {
+    var a: Int = 10
+}
