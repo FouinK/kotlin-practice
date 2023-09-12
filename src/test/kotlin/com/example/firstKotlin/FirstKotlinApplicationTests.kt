9586b3abd6f83623cd21a238ea19ac9838d86b8b
Fouink <yyss9103@gmail.com>
2023-09-12 20:54:42 +0900

    List<Int?>? 가능

--- src/test/kotlin/com/example/firstKotlin/FirstKotlinApplicationTests.kt
+++ src/test/kotlin/com/example/firstKotlin/FirstKotlinApplicationTests.kt
@@ -7,27 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest
 class FirstKotlinApplicationTests {
 	@Test
 	fun contextLoads() {
-		val oldMap = mutableMapOf<Int, String>()
-		oldMap.put(1, "STring")
-		oldMap[2] = "123"
-
-		val newMap = mapOf(1 to "String", 2 to "String2")
-
-		for (mutableEntry in oldMap) {
-			println(mutableEntry.key)
-			println(mutableEntry.value)
-			println(oldMap.get(mutableEntry.key))
-		}
-
-		for (key in newMap.keys) {
-			println(key)
-			println(newMap[key])
-		}
-
-		for ((key, value) in oldMap.entries) {
-			println(key)
-			println(value)
-		}
+		var emptyList: List<Int?>? = mutableListOf()
+		val emptyList2: List<Int?> = mutableListOf()
 	}
 
 	private fun printNumbers(numbers: List<Int>) {
