53d510c1eb265991fe921c7d3f9019b7bbafe91b
Fouink <yyss9103@gmail.com>
2023-09-14 21:24:32 +0900

    class는 필드에 대해서 자동으로 componentN을 만들어줌

--- src/test/kotlin/com/example/firstKotlin/FirstKotlinApplicationTests.kt
+++ src/test/kotlin/com/example/firstKotlin/FirstKotlinApplicationTests.kt
@@ -8,7 +8,9 @@ class FirstKotlinApplicationTests {
 	@Test
 	fun contextLoads() {
 		val person = Person("윤성현", 26)
-		val (name, age) = person
+//		val (name, age) = person
+		val name = person.component1()
+		val age = person.component2()
 
 		println("name = ${name}")
 		println("age = ${age}")
